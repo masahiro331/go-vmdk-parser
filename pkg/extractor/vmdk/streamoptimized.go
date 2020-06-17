ff5581d546b1b2b1057470deb21bf176b5bb5085
masahiro331 <mur4m4s4.331@gmail.com>
2020-06-17 19:41:26 +0900

    Delete un used program

--- pkg/extractor/vmdk/streamoptimized.go
+++ pkg/extractor/vmdk/streamoptimized.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"compress/zlib"
 	"encoding/binary"
-	"fmt"
 	"io"
 	"log"
 	"math"
@@ -77,9 +76,6 @@ func (s *streamOptimizedExtentReader) Read(p []byte) (int, error) {
 	}
 
 	for {
-		// fmt.Printf("filesectorpos: %d\n", s.fileSectorPos)
-		// fmt.Printf("sectorpos    : %d\n", s.sectorPos)
-		// fmt.Printf("buffer len   : %d\n", s.buffer.Len())
 		if s.buffer.Len() == 0 {
 			s.fileSectorPos = s.sectorPos + CLUSTER_SIZE
 			_, err := s.readGrainData()
@@ -120,93 +116,8 @@ func (s *streamOptimizedExtentReader) Read(p []byte) (int, error) {
 			}
 			return i, err
 		}
-
-		// i, err := s.buffer.Read(p)
-		// fmt.Printf("SectorPos: %d\n", s.sectorPos)
-		// fmt.Printf("fileSectorPos: %d\n", s.fileSectorPos)
-		// fmt.Printf("startSector: %d\n", s.partition.StartSector)
-		// if err != nil {
-		// 	return i, err
-		// }
-		// return i, nil
-
-		// if s.buffer.Len()+s.secondbuffer.Len() < len(p) {
-		// 	if s.buffer.Len() == 0 {
-		// 		_, err := s.readGrainData()
-		// 		if s.partition != nil && s.sectorPos == uint64(s.partition.StartSector+s.partition.Size) {
-		// 			return 0, io.EOF
-		// 		}
-		// 		// s.fileSectorPos = s.fileSectorPos + CLUSTER_SIZE
-		// 		if err != nil {
-		// 			if err == io.EOF {
-		// 				s.err = err
-		// 			}
-		// 			return 0, err
-		// 		}
-		// 	}
-		// 	if s.fileSectorPos != s.sectorPos { // + CLUSTER_SIZE {
-		// 		s.secondbuffer.Write(make([]byte, SECOTR_SIZE*CLUSTER_SIZE))
-		// 		s.fileSectorPos = s.fileSectorPos + CLUSTER_SIZE
-		// 	} else {
-		// 		s.buffer.WriteTo(s.secondbuffer)
-		// 		// Test code
-		// 		if s.buffer.Len() != 0 {
-		// 			panic("buffer is not empty")
-		// 		}
-		// 	}
-		// } else {
-		// 	if s.secondbuffer.Len() != 0 {
-		// 		n, err := s.secondbuffer.Read(p)
-		// 		if err != nil {
-		// 			log.Println(err)
-		// 		}
-		// 		return n, err
-		// 	} else {
-		// 		n, err := s.buffer.Read(p)
-		// 		if err != nil {
-		// 			log.Println(err)
-		// 		}
-		// 		if s.buffer.Len() == 0 && s.err == io.EOF {
-		// 			return 0, s.err
-		// 		}
-		// 		return n, err
-		// 	}
-		// }
-
-		// if s.buffer.Len() < len(p) {
-		// 	if s.fileSectorPos != s.sectorPos+CLUSTER_SIZE {
-		// 		s.buffer.Write(make([]byte, SECOTR_SIZE*CLUSTER_SIZE))
-		// 		s.fileSectorPos = s.fileSectorPos + CLUSTER_SIZE
-		// 	} else {
-		// 		_, err := s.readGrainData()
-		// 		if s.partition != nil && s.sectorPos == uint64(s.partition.StartSector+s.partition.Size) {
-		// 			return 0, io.EOF
-		// 		}
-		// 		s.fileSectorPos = s.fileSectorPos + CLUSTER_SIZE
-		// 		if err != nil {
-		// 			if err == io.EOF {
-		// 				s.err = err
-		// 			}
-		// 			return 0, err
-		// 		}
-		// 	}
-		// } else {
-		// 	n, err := s.buffer.Read(p)
-		// 	if err != nil {
-		// 		log.Println(err)
-		// 	}
-		// 	if s.buffer.Len() == 0 && s.err == io.EOF {
-		// 		return 0, s.err
-		// 	}
-		// 	return n, err
-		// }
 	}
 
-	// bufが無くなったら補充する機能
-	// あとどれくらい返せばいいのか？
-	// bufferより大きなサイズを求められた時
-
-	// 言われたサイズを返す機能
 }
 
 func (s *streamOptimizedExtentReader) Next() (*disk.Partition, error) {
@@ -307,115 +218,8 @@ func (s *streamOptimizedExtentReader) readGrainData() (uint64, error) {
 }
 
 func (s *streamOptimizedExtentReader) Close() (err error) {
-
+	// TODO: close
 	return nil
-}
-
-func (s *StreamOptimizedExtent) ExtractFromFile(r io.Reader, filenames []string) (FileMap, error) {
-	sector := make([]byte, Sector)
-
-	// Trim vmdk head Metadata
-	overHeadOffset := s.header.OverHead - s.header.DescriptorOffset - s.header.DescriptorSize
-	for i := uint64(0); i < (overHeadOffset); i++ {
-		if _, err := r.Read(sector); err != nil {
-			return nil, xerrors.Errorf("failed to read overhead error: %w", err)
-		}
-	}
-
-	filemap := make(map[string][][]byte)
-
-	var partitionOffset = -1
-	var mbr *disk.MasterBootRecord
-	for {
-		if _, err := r.Read(sector); err != nil {
-			return nil, xerrors.Errorf("failed to read marker error: %w", err)
-		}
-
-		m := parseMarker(sector)
-
-		switch m.Type {
-		case MARKER_GRAIN:
-			buf := new(bytes.Buffer)
-			if m.Size < 500 {
-				buf.Write(m.Data[:m.Size])
-			} else {
-				buf.Write(m.Data)
-				limit := uint64(math.Ceil(float64(m.Size-500) / float64(Sector)))
-				for i := uint64(0); i < limit; i++ {
-					if _, err := r.Read(sector); err != nil {
-						return nil, xerrors.Errorf("failed to read Grain Data error: %w", err)
-					}
-					buf.Write(sector)
-				}
-			}
-
-			if partitionOffset < 0 {
-				zr, err := zlib.NewReader(buf)
-				if err != nil {
-					return nil, xerrors.Errorf("failed to read zlib error: %w", err)
-				}
-				// Read Master Boot Record
-				// TODO: Support GPT disk type
-				mbr, err = disk.NewMasterBootRecord(zr)
-				if err != nil {
-					return nil, xerrors.Errorf("failed to parse disk error: %w", err)
-				}
-				for i, p := range mbr.Partitions {
-					fmt.Printf("%d: ss(%d), size(%d)\n", i, p.GetStartSector(), p.GetSize())
-				}
-
-				partitionOffset = partitionOffset + 1
-
-			} else {
-				// Check Partition
-				if m.Value == uint64(mbr.Partitions[partitionOffset].GetSize()+mbr.Partitions[partitionOffset].GetStartSector()) {
-					partitionOffset = partitionOffset + 1
-				}
-
-				fn := fmt.Sprintf("%d.img", partitionOffset)
-				filemap[fn] = append(filemap[fn], buf.Bytes())
-			}
-
-		case MARKER_EOS:
-		case MARKER_GT:
-			// GRAIN TABLE always 512 entries
-			// GRAIN TABLE ENTRY is 32bit
-			// GRAIN TABLE is 2KB
-			for i := uint64(0); i < m.Value; i++ {
-				if _, err := r.Read(sector); err != nil {
-					return nil, xerrors.Errorf("failed to read Grain Table error: %w", err)
-				}
-			}
-		case MARKER_GD:
-			for i := uint64(0); i < m.Value; i++ {
-				if _, err := r.Read(sector); err != nil {
-					return nil, xerrors.Errorf("failed to read Grain Directory error: %w", err)
-				}
-			}
-		case MARKER_FOOTER:
-			return filemap, nil
-		default:
-			return nil, xerrors.New("Invalid Marker Type")
-		}
-	}
-}
-
-func parseMarker(sector []byte) *Marker {
-	size := binary.LittleEndian.Uint32(sector[8:12])
-	if size == 0 {
-		return &Marker{
-			Value: binary.LittleEndian.Uint64(sector[:8]),
-			Size:  size,
-			Type:  binary.LittleEndian.Uint32(sector[12:16]),
-		}
-	} else {
-		return &Marker{
-			Value: binary.LittleEndian.Uint64(sector[:8]),
-			Size:  size,
-			Type:  MARKER_GRAIN,
-			Data:  sector[12:],
-		}
-	}
 }
 
 /*
@@ -437,4 +241,22 @@ type Marker struct {
 	Size  uint32
 	Type  uint32
 	Data  []byte
+}
+
+func parseMarker(sector []byte) *Marker {
+	size := binary.LittleEndian.Uint32(sector[8:12])
+	if size == 0 {
+		return &Marker{
+			Value: binary.LittleEndian.Uint64(sector[:8]),
+			Size:  size,
+			Type:  binary.LittleEndian.Uint32(sector[12:16]),
+		}
+	} else {
+		return &Marker{
+			Value: binary.LittleEndian.Uint64(sector[:8]),
+			Size:  size,
+			Type:  MARKER_GRAIN,
+			Data:  sector[12:],
+		}
+	}
 }
--- pkg/extractor/vmdk/vmdk.go
+++ pkg/extractor/vmdk/vmdk.go
@@ -33,14 +33,6 @@ type Header struct {
 	Padding            [433]byte
 }
 
-type FileMap map[string][][]byte
-
-// Expected CompressedGrainData or GrainData
-type VMDK interface {
-	// Extract(ctx context.Context, imageName string, filenames []string) (FileMap, error)
-	ExtractFromFile(r io.Reader, filenames []string) (FileMap, error)
-}
-
 type Reader interface {
 	io.ReadCloser
 	Next() (*disk.Partition, error)
@@ -72,47 +64,6 @@ func NewReader(r io.Reader, dict []byte) (Reader, error) {
 	}
 
 	return readerFunc(r, dict, header)
-}
-
-// WARN: NewVMDKReader read Header
-func NewExtractor(r io.Reader) (VMDK, error) {
-	var header Header
-	if err := binary.Read(r, binary.LittleEndian, &header); err != nil {
-		return nil, xerrors.Errorf("failed to read binary error: %w", err)
-	}
-
-	// TODO: RAW extent data file is unsupported
-	if header.DescriptorOffset == 0 {
-		return nil, xerrors.New("Unsupported vmdk format")
-	}
-
-	sector := make([]byte, Sector)
-	var embDescriptor string
-	for i := uint64(0); i < header.DescriptorSize; i++ {
-		if _, err := r.Read(sector); err != nil {
-			log.Fatal(err)
-		}
-		embDescriptor = embDescriptor + string(sector)
-	}
-
-	extent, err := newExtent(embDescriptor, header)
-	if err != nil {
-		return nil, xerrors.Errorf("failed to new vmdk reader: %w", err)
-	}
-
-	return extent, nil
-}
-
-// TODO: Parse EmbededDescriptor
-// https://www.vmware.com/support/developer/vddk/vmdk_50_technote.pdf
-func newExtent(s string, header Header) (VMDK, error) {
-	if strings.Contains(s, `createType="streamOptimized"`) {
-		return &StreamOptimizedExtent{
-			header: header,
-		}, nil
-	} else {
-		return nil, xerrors.New("Unsupported createType")
-	}
 }
 
 func newReaderFunc(s string, header Header) (func(r io.Reader, dict []byte, header Header) (Reader, error), error) {
